684755878e0bbb438c579fdd97971e45f09c0871
LizethVictoria20 <lizvictoria754@gmail.com>
2024-09-19 10:56:02 -0500

    Agregando imagen de header

--- static/js/main.js
+++
@@ -1,9 +0,0 @@
-// Conectar al servidor Socket.IO con WebSocket y fallback a polling
-const socket = io("https://visaparaturistas.onrender.com", {
-  transports: ["websocket", "polling"],
-});
-
-// Escuchar eventos de conexión
-socket.on("connect", () => {
-  console.log("Conectado al servidor");
-});
